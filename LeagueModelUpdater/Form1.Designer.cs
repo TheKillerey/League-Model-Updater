d091bf7a7e55e5e580e3bda6b0e911ec4707fd44
The Killerey <thekillerey@gmail.com>
2021-02-21 14:08:42 +0100

    * Updating to latest LeagueToolbox version. * Complex skins can broke. Because of that I added support for export skn/skl only. * Added more error informations. More soon * Added early support for MAPGEO2GLTF

--- LeagueModelUpdater/Form1.Designer.cs
+++ LeagueModelUpdater/Form1.Designer.cs
@@ -43,12 +43,22 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.button5 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(288, 83);
+            this.button1.Location = new System.Drawing.Point(288, 138);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(105, 23);
             this.button1.TabIndex = 0;
@@ -58,7 +68,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(35, 139);
+            this.button2.Location = new System.Drawing.Point(416, 109);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(146, 29);
             this.button2.TabIndex = 1;
@@ -68,15 +78,19 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(161, 25);
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Location = new System.Drawing.Point(161, 80);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(232, 23);
             this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.textbox1_dragdrop);
+            this.textBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.textbox1_dragenter);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(161, 54);
+            this.textBox2.Location = new System.Drawing.Point(161, 109);
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
             this.textBox2.Size = new System.Drawing.Size(232, 23);
@@ -85,7 +99,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Location = new System.Drawing.Point(12, 80);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(124, 15);
             this.label1.TabIndex = 4;
@@ -94,7 +108,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 57);
+            this.label2.Location = new System.Drawing.Point(12, 112);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(144, 15);
             this.label2.TabIndex = 5;
@@ -102,7 +116,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(221, 139);
+            this.button3.Location = new System.Drawing.Point(416, 74);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(146, 29);
             this.button3.TabIndex = 6;
@@ -117,15 +131,16 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(477, 335);
+            this.label3.Location = new System.Drawing.Point(832, 517);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(104, 15);
             this.label3.TabIndex = 7;
-            this.label3.Text = "v1.1 by TheKillerey";
+            this.label3.Text = "v1.2 by TheKillerey";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(288, 250);
+            this.button4.Location = new System.Drawing.Point(288, 427);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(105, 23);
             this.button4.TabIndex = 8;
@@ -135,7 +150,7 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(161, 221);
+            this.textBox3.Location = new System.Drawing.Point(161, 398);
             this.textBox3.Name = "textBox3";
             this.textBox3.ReadOnly = true;
             this.textBox3.Size = new System.Drawing.Size(232, 23);
@@ -145,7 +160,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 224);
+            this.label4.Location = new System.Drawing.Point(12, 406);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(113, 15);
             this.label4.TabIndex = 10;
@@ -153,7 +168,7 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(221, 294);
+            this.button5.Location = new System.Drawing.Point(416, 394);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(146, 29);
             this.button5.TabIndex = 11;
@@ -161,13 +176,112 @@
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 487);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 15);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "MAPGEO to GLTF";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(585, 73);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(146, 29);
+            this.button6.TabIndex = 14;
+            this.button6.Text = "Export only SKN";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(585, 108);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(146, 29);
+            this.button7.TabIndex = 15;
+            this.button7.Text = "Export only SKL";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.AllowDrop = true;
+            this.textBox4.Location = new System.Drawing.Point(161, 479);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(232, 23);
+            this.textBox4.TabIndex = 16;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(288, 508);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(105, 23);
+            this.button8.TabIndex = 17;
+            this.button8.Text = "Load MAPGEO";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(416, 475);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(146, 29);
+            this.button9.TabIndex = 18;
+            this.button9.Text = "Create GLTF / GLB";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(12, 357);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(117, 23);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Currently WIP";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(41, 47);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(589, 15);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "If your skin file is broken ingame, use only \"Export only SKL\". We are working on" +
+    " that problem right now.";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(26, 26);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AccessibleDescription = "";
             this.AccessibleName = "";
+            this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(593, 359);
+            this.ClientSize = new System.Drawing.Size(948, 541);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox3);
@@ -183,8 +297,11 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "League Model Updater";
+            this.Text = "League File Toolbox";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -205,6 +322,15 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- LeagueModelUpdater/Form1.cs
+++ LeagueModelUpdater/Form1.cs
@@ -4,6 +4,9 @@ using System;
 using System.IO;
 using System.Windows.Forms;
 using SharpGLTF.Schema2;
+using Newtonsoft.Json.Schema;
+using System.Runtime.CompilerServices;
+using LeagueToolkit.IO.MapGeometry;
 
 namespace LeagueModelUpdater
 {
@@ -14,10 +17,18 @@ namespace LeagueModelUpdater
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.AllowDrop = true;
+        }
+        
         OpenFileDialog skn = new OpenFileDialog();
         //string  skl = skn.FileName();
         OpenFileDialog glb = new OpenFileDialog();
         SaveFileDialog saveskn = new SaveFileDialog();
+        OpenFileDialog openmapgeo = new OpenFileDialog();
+        SaveFileDialog savegltf = new SaveFileDialog();
+        SaveFileDialog saveglb = new SaveFileDialog();
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -56,32 +67,32 @@ namespace LeagueModelUpdater
                 skeletonfile.Write(outputSKL);
 
                 //Fixes the error that is caused by Crauzer :v
-                byte[] find = {0x00, 0x00, 0x6F, 0x74};
-                byte[] replace = {0x72, 0x6F, 0x6F, 0x74};
-                byte[] file = File.ReadAllBytes(outputSKL);
-                int i, j, iMax = file.Length - find.Length ;
-                for (i = 0; i <= iMax; i++)
-                {
-                  for (j = 0; j < find.Length; j++)
-                    if (file[i + j] != find[j]) break;
-                  if (j == find.Length) break;
-                }
-                if (i <= iMax)
-                {
-                  for (j = 0; j < find.Length; j++)
-                    file[i + j] = replace[j];
-                  File.WriteAllBytes(outputSKL, file);
-                }
-
+                //byte[] find = {0x00, 0x00, 0x6F, 0x74};
+                //byte[] replace = {0x72, 0x6F, 0x6F, 0x74};
+                //byte[] file = File.ReadAllBytes(outputSKL);
+                //int i, j, iMax = file.Length - find.Length ;
+                //for (i = 0; i <= iMax; i++)
+                //{
+                //  for (j = 0; j < find.Length; j++)
+                //    if (file[i + j] != find[j]) break;
+                //  if (j == find.Length) break;
+                //}
+                //if (i <= iMax)
+                //{
+                //  for (j = 0; j < find.Length; j++)
+                //    file[i + j] = replace[j];
+                //  //File.WriteAllBytes(outputSKL, file);
+                //}
+                MessageBox.Show("Your League Skin is now updated to the latest League version.", $"Files Exported!", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
 
-            catch (FileNotFoundException ex)
+            catch (Exception ex)
             {
 
                 MessageBox.Show("Skeleton Missing. Your SKN file needs a skeleton (SKL).", $"Error: Can't find .skl file!", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
             }
-            MessageBox.Show("Your League Skin is now updated to the latest League version.", $"Files Exported!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            
 
         }
 
@@ -99,31 +110,32 @@ namespace LeagueModelUpdater
                 skinfile2.Write(sknfile);
                 skeletonfile2.Write(sklfile);
 
-                byte[] find = {0x00, 0x00, 0x6F, 0x74};
-                byte[] replace = {0x72, 0x6F, 0x6F, 0x74};
-                byte[] file = File.ReadAllBytes(sklfile);
-                int i, j, iMax = file.Length - find.Length ;
-                for (i = 0; i <= iMax; i++)
-                {
-                  for (j = 0; j < find.Length; j++)
-                    if (file[i + j] != find[j]) break;
-                  if (j == find.Length) break;
-                }
-                if (i <= iMax)
-                {
-                  for (j = 0; j < find.Length; j++)
-                    file[i + j] = replace[j];
-                  File.WriteAllBytes(sklfile, file);
-                }
+                //byte[] find = {0x00, 0x00, 0x6F, 0x74};
+                //byte[] replace = {0x72, 0x6F, 0x6F, 0x74};
+                //byte[] file = File.ReadAllBytes(sklfile);
+                //int i, j, iMax = file.Length - find.Length ;
+                //for (i = 0; i <= iMax; i++)
+                //{
+                //  for (j = 0; j < find.Length; j++)
+                //    if (file[i + j] != find[j]) break;
+                //  if (j == find.Length) break;
+                //}
+                //if (i <= iMax)
+                //{
+                //  for (j = 0; j < find.Length; j++)
+                //    file[i + j] = replace[j];
+                //  File.WriteAllBytes(sklfile, file);
+                //}
+                MessageBox.Show("Your League Skin is now updated to the latest League version.", $"Files Exported!", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
 
-            catch (FileNotFoundException ex)
+            catch (Exception ex)
             {
 
                 MessageBox.Show("Skeleton Missing. Your SKN file needs a skeleton (SKL).", $"Error: Can't find .skl file!", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
             }
-            MessageBox.Show("Your League Skin is now updated to the latest League version.", $"Files Exported!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            
         }
 
         
@@ -153,41 +165,32 @@ namespace LeagueModelUpdater
             saveskn.Filter = "LeagueSkinFile (*.skn)| *.skn";
                 if (saveskn.ShowDialog() == DialogResult.OK)
                 {
-                
+                    try
+                    { 
                     string GLTFFile = textBox3.Text;
                     var model = ModelRoot.Load(GLTFFile);
                     var gltf2skn = SimpleSkinGltfExtensions.ToLeagueModel(model);
+               
                 
                     string outputskn = saveskn.FileName;
                     string SKNText = Path.GetFullPath(saveskn.FileName);
                 
                     string SKLText = SKNText.Replace(".skn", ".skl");
                     string outputskl = SKLText;
-                
                     
                 
                     gltf2skn.Item1.Write(outputskn);
                     gltf2skn.Item2.Write(outputskl);
+                    }
+                    catch (Exception ag)
+                    {
+                    MessageBox.Show("Make sure you load a file before exporting it!", $"Error: No file is loaded!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
                     
-                    byte[] find = {0x00, 0x00, 0x6F, 0x74};
-                    byte[] replace = {0x72, 0x6F, 0x6F, 0x74};
-                    byte[] file = File.ReadAllBytes(outputskl);
-                    int i, j, iMax = file.Length - find.Length ;
-                    for (i = 0; i <= iMax; i++)
-                    {
-                      for (j = 0; j < find.Length; j++)
-                        if (file[i + j] != find[j]) break;
-                      if (j == find.Length) break;
-                    }
-                    if (i <= iMax)
-                    {
-                      for (j = 0; j < find.Length; j++)
-                        file[i + j] = replace[j];
-                      File.WriteAllBytes(outputskl, file);
-                    }
-            }
 
+                }
 
+            
 
 
 
@@ -195,6 +198,146 @@ namespace LeagueModelUpdater
            
         }
 
-        
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textbox1_dragdrop(object sender, DragEventArgs e)
+        {
+            var data = e.Data.GetData(DataFormats.FileDrop);
+            if(data != null)
+            {
+                
+                var fileNames = data as string[];
+                string ext = Path.GetExtension(fileNames[0]);
+                if(fileNames.Length > 0)
+                   if(ext.Contains(".skn"))
+                    {
+                      textBox1.Text = Path.GetFullPath(fileNames[0]);
+                      var textbox2new = textBox1.Text;
+                      var skltextbox = textbox2new.Replace(".skn", ".skl");
+                      textBox2.Text = skltextbox;
+                    }
+                   else
+                    {
+                        MessageBox.Show("Please import only skn (League's SimpleSkin)!", "Wrong File",MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                   
+            }  
+        }
+
+        private void textbox1_dragenter(object sender, DragEventArgs e)
+        {
+            e.Effect=DragDropEffects.Copy;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e) //Export Only SKN
+        {
+          try
+            {
+                
+                string sknfile = textBox1.Text;
+                SimpleSkin skinfile = new(sknfile);
+                string outputSKN = Path.ChangeExtension(sknfile, "new.skn");
+                skinfile.Write(outputSKN);
+                MessageBox.Show("Your LeagueSkin is now updated to the latest League version. If it's broken please use only skl as update!", $"Files Exported!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            catch (FileNotFoundException ex)
+            {
+
+                MessageBox.Show("Skeleton Missing. Your SKN file needs a skeleton (SKL).", $"Error: Can't find .skl file!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            catch (Exception ag)
+                {
+                MessageBox.Show("Make sure you load a file before exporting it!", $"Error: No file is loaded!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            
+        }
+
+        private void button7_Click(object sender, EventArgs e) //Export Only SKL
+        {
+            try
+            {
+                
+                string sklfile = textBox2.Text;
+                Skeleton skeletonfile = new Skeleton(sklfile);
+                string outputSKL = Path.ChangeExtension(sklfile, "new.skl");
+                skeletonfile.Write(outputSKL);
+                MessageBox.Show("Your Skeleton is now updated to the latest League version.", $"Files Exported!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            catch (FileNotFoundException ex)
+            {
+
+                MessageBox.Show("Skeleton Missing. Your SKN file needs a skeleton (SKL).", $"Error: Can't find .skl file!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            catch (Exception ag)
+                {
+                MessageBox.Show("Make sure you load a file before exporting it!", $"Error: No file is loaded!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            openmapgeo.Filter = "LeagueMapFile (*.mapgeo)| *.mapgeo";
+            if (openmapgeo.ShowDialog() == DialogResult.OK)
+            {
+                textBox4.Text = openmapgeo.FileName;
+                string MAPGEOText = Path.GetFullPath(openmapgeo.FileName);
+
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            string MAPGEOText = Path.GetFullPath(openmapgeo.FileName);
+            
+
+            savegltf.Filter = "GLTF|*.gltf|GLB|*.glb";
+            if (savegltf.ShowDialog() == DialogResult.OK)
+
+            {
+                string outputgltf = Path.GetFullPath(savegltf.FileName);
+                try
+            {
+
+                MapGeometry mapgeo = new MapGeometry(MAPGEOText);
+                var convertmap = mapgeo.ToGLTF();
+                    convertmap.SaveGLB(outputgltf);
+                MessageBox.Show("Exported to GLTF/GLB.", $"Files Exported!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show("MapFile is missing. Make sure you load the correct MAPGEO file!", $"Error: Can't find .mapgeo file!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            catch (System.ArgumentException ag)
+                {
+                MessageBox.Show("Make sure you load a file before exporting it!", $"Error: No file is loaded!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                
+            }
+
+
+            
+            
+        }
     }
 }
